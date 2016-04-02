6c05ba99ae4903d9946a931a6cb85bb4b2c97c00
Cara Canady <cara.canady@gmail.com>
2016-04-02 18:02:36 -0400

    list shows total doesn't update wtf

--- lab6java.js
+++ lab6java.js
@@ -21,32 +21,32 @@ var shoppingList = [
 
 var total = 0;
 
-
-// shoppinglist.forEach(function(element){
-// 	console.log(element.name + " $"  + element.price.toFixed(2));
-// 	total += element.price;
-// });
-
-// console.log("Total: $" + total.toFixed(2));
-
 shoppingList.forEach(function(element){
-	var newElement = document.createElement('p');
+	var newElement = document.createElement('li');
 	newElement.innerHTML = element.name +"... $" + element.price;
-	document.body.appendChild(newElement);
+	document.getElementById('list').appendChild(newElement);
 	total += element.price;
 });
 
-var subtotal = total;
 newPrice = document.createElement('p');
-newPrice.innerHTML = "TOTAL is " + total;
-document.body.appendChild(newPrice);
+newPrice.innerHTML = "TOTAL is $" + total;
+document.getElementById('total').appendChild(newPrice);
 
+// Here's my function
 function myFunction() {
-        var newName = document.getElementById("name").value;
-        var newItem = document.getElementById("item").value;
-        var newThing = newName + newItem;
-        newThing.innerHTML = newName + "... $" + newItem;
-        document.body.appendChild (newThing);
-        
+      	var newName = document.getElementById("name").value;
+       	var newNum = document.getElementById("cost").value;
+       	var newCost = Number(newNum);
 
-}
+       	var newElement = document.createElement('li');
+		newElement.innerHTML = newName +"... $" + newCost;
+		document.getElementById('list').appendChild(newElement);
+
+togetherTotal = 0;
+togetherTotal = total += newCost;		
+		// function total () {
+		// 	newPrice = document.createElement('p');
+		// 	newPrice.innerHTML = "TOTAL is $" += total +=newCost;
+		// 	document.getElementById('total').appendChild(newPrice);
+		};
+
